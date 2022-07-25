078ea3c0a4769bf42f738db9ac0c230457a803d1
Marilynn Stone <hello@marilynnstone.com>
2022-07-25 03:29:56 +0000

    Change POST to allow tweet to show on page without refresh. Add safeText function to escape text to prevent XSS.

--- public/scripts/client.js
+++ public/scripts/client.js
@@ -1,38 +1,58 @@
 /* eslint-disable no-undef */
 
-$(document).ready(function () {
+$(document).ready(function() {
   console.log("client is ready");
 
-  const $createTweetElement = (tweetData) => {
-    if (tweetData) {
-      const $tweet = `
-      <article class="tweet">
-        <header> 
-          <span><i ${tweetData.user.avatars}></i>${tweetData.user.name}</span>
-          <p>${tweetData.user.handle}</p>
-        </header>
-        <div class="tweet-content">${tweetData.content.text}</div>
-        <footer>
-          <time>${timeago.format(tweetData.created_at)}</time>
-          <nav>
-          <i class="fa-solid fa-flag"></i>
-          <i class="fa-solid fa-retweet"></i>
-          <i class="fa-solid fa-heart"></i>
-        </nav>
-        </footer>
-      </article>
-    `;
-      return $tweet;
-    }
-  };
+  $loadTweets();
+
+  $newTweet();
+
   
-  const $renderTweets = (tweets) => {
-    for (const tweet of tweets) {
-      const newTweet = $createTweetElement(tweet);
-      $('#tweets-container').append(newTweet);
-    }
-  };
-  
+
+});
+
+const safeHTML = function(str) {
+  let div = document.createElement("div");
+  div.appendChild(document.createTextNode(str));
+  return div.innerHTML;
+};
+
+const $createTweetElement = (tweetData) => {
+  const $tweet = `
+    <article class="tweet">
+      <header> 
+        <span><i ${tweetData.user.avatars}></i>${tweetData.user.name}</span>
+        <p>${tweetData.user.handle}</p>
+      </header>
+      <div class="tweet-content">${safeHTML(tweetData.content.text)}</div>
+      <footer>
+        <time>${timeago.format(tweetData.created_at)}</time>
+        <nav>
+        <i class="fa-solid fa-flag"></i>
+        <i class="fa-solid fa-retweet"></i>
+        <i class="fa-solid fa-heart"></i>
+      </nav>
+      </footer>
+    </article>
+  `;
+  return $tweet;
+};
+
+const $renderTweets = (tweets) => {
+  for (const tweet of tweets) {
+    const newTweet = $createTweetElement(tweet);
+    $('#tweets-container').prepend(newTweet);
+  }
+};
+
+const $loadTweets = () => {
+  $.get("/tweets")
+    .then(data => {
+      $renderTweets(data);
+    });
+};
+
+const $newTweet = () => {
   $("form").on("submit", function(event) {
     event.preventDefault();
     if ($("form textarea").val().length === 0) {
@@ -41,12 +61,10 @@ $(document).ready(function () {
     if ($("form textarea").val().length > 140) {
       alert("Your tweet is too long to submit.");
     }
-    $.ajax("/tweets", { method: "POST", data:$(this).serialize()});
+    $.post("/tweets", $(this).serialize())
+      .then(() => {
+        $loadTweets();
+      });
+    location.reload();
   });
-
-  $.get("/tweets")
-    .then(data => {
-      $renderTweets(data);
-    });
-
-});
+};
